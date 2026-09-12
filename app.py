90c0824ffe9d0d19f16bed1ed770b7350eb985d6
Alina <areebasizzlingish@gmail.com>
2026-09-12 18:28:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from news_api import fetch_news
 from gnews_api import fetch_gnews
+from open_meteo_api import fetch_weather
 from retrieval import retrieve_articles
 from ai import generate_intelligence
 
